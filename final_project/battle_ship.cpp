f62b4b6eb75686ffb5aa37b31aa88e2463b1917a
Eric Yun <eyun89@gmail.com>
2019-07-24 21:49:46 -0700

    final_project - not done

--- final_project/battle_ship.cpp
+++ final_project/battle_ship.cpp
@@ -1,38 +1,362 @@
+/**************************************************************************
+ *	AUTHOR	         : Eric Yun
+ *	FINAL PROJECT	 : Battleship
+ *	CLASS	         : CS 002
+ *	SECTION          : MTRF: 7:00a - 12p
+ *	Due Date         : June 20, 2019	
+ **************************************************************************/
 #include "battle_ship.h"
+#include <fstream>
+
+/**************************************************************************
+ *
+ * BATTLESHIP
+ *
+ *--------------------------------------------------------------------------
+ * This program emulates the game of Battleship
+ *--------------------------------------------------------------------------
+ * INPUT:
+ *
+ * OUTPUT:
+ *
+ ***************************************************************************/
+
+// this is how to print out the vectors
+// cout << p1.fleet[4].space[0].cols;
 
 int main()
 {
-	char playerHidden[SIZE][SIZE];
-	char playerReveal[SIZE][SIZE];
-	char compHidden[SIZE][SIZE];
-	char compReveal[SIZE][SIZE];
+	PlayerBoard p1;
+	PlayerBoard p2;
 
-	initBoards(playerHidden,
-			   playerReveal,
-			   compHidden,
-			   compReveal);
-	displayBoards(playerHidden,
-				  compHidden);
+	// initialize boards
+	initBoard(p1.board, p2.board);
+	initFleet(p1);
+	initFleet(p2);
+	displayBoards(p1.board, p2.board);
+	
+	// initialize player 1
+	cout << "Player 1 set your board." << endl;
+	for (int i = 0; i < 5; i++)
+	{
+		getValidShipInfo(p1, i);
+		displayBoards(p1.board, p2.board);
+	}
+
+	// initialize player 2
+	cout << string(4, '\n');
+	cout << "Player 2 set your board." << endl;
+	for (int i = 0; i < 5; i++)
+	{
+		getValidShipInfo(p2, i);
+		displayBoards(p1.board, p2.board);
+	}
+
+
+	// start game
+	int PLAYER_1 = 1;
+	int PLAYER_2 = 2;
+	int turn = PLAYER_1;
+
+
+	// hit target
+	while (!gameOver(p1, p2))
+	{
+		if (turn == PLAYER_1)
+		{
+			fireShot(p1, p2, turn);
+			displayBoards(p1.board, p2.board);
+		}
+
+		else if (turn == PLAYER_2)
+		{
+			fireShot(p2, p1, turn);
+			displayBoards(p1.board, p2.board);
+		}
+		switchPlayers(turn);
+	}
+}
+
+bool gameOver(PlayerBoard &p1, PlayerBoard &p2)
+{
+	int p1Total;
+	int p2Total;
+	for (int i = 0; i < FLEET_SIZE; i++)
+	{
+		p1Total += p1.fleet[i].hitCount;
+	}
+
+	for (int i = 0; i < FLEET_SIZE; i++)
+	{
+		p2Total += p2.fleet[i].hitCount;
+	}
+
+	if (p1Total == TOTAL_HIT_COUNT ||
+		p2Total == TOTAL_HIT_COUNT)
+		return true;
+	return false;
+
+}
+
+void fireShot(PlayerBoard &origin, 
+		      PlayerBoard &target, 
+		      int turn)
+{
+	char ch;				// INPUT - row char
+	int row, col;			// tart hit row and col
+	int shipIdx;			// ship index
+	int targetHitCount;	    // hit count per ship
+	int shipHitCapacity;	// ship hit capacity
+	string targetShipName;	// ship name
+
+	cout << "Player " << turn << ": \n";
+	cout << "Fire a shot: ";
+	cin >> ch >> col;
+	row = ch - 65;
+	col = col - 1;
+	if (isHit(target, row, col))
+	{
+		shipIdx = targetHit(target, row, col);
+		targetHitCount = target.fleet[shipIdx].hitCount;
+		targetHitCount++;
+
+		shipHitCapacity = target.fleet[shipIdx].size;
+
+		targetShipName = target.fleet[shipIdx].name;
+		cout << "ship name: " << targetShipName << endl;
+		cout << "ship hit capacity: " << shipHitCapacity << endl;
+		cout << "shit hit count: " << targetHitCount << endl;
+		if (targetHitCount == shipHitCapacity)
+			cout << "You sunk the "
+				 << targetShipName << "!!!\n";
+	}
+	else
+	{
+		cout << "You Missed.\n";
+	}
+
+
+	
+}
+
+
+// return the index of the ship that has been hit
+int targetHit(PlayerBoard &p,
+			  int row,
+			  int col)
+{
+	int shipSize;
+	int targetRow;
+	int targetCol;
+	for (int i = 1; i < FLEET_SIZE; i++)
+	{
+		shipSize = p.fleet[i].space.size();
+		for (int j = 0; j < shipSize; j++)
+		{
+			targetRow = p.fleet[i].space[j].rows;
+			targetCol = p.fleet[i].space[j].cols;
+			if (row == targetRow &&
+				col == targetCol)
+				return i;
+		}
+	}
+	return -1;
+}
+
+bool isHit(PlayerBoard &p, int row, int col)
+{
+	if (p.board[row][col] == 'S')
+	{
+		p.board[row][col] = 'H';
+		return true;
+	}
+
+	else if (p.board[row][col] == 'H')
+	{
+		return false;
+	}
+	else 
+		p.board[row][col] = 'M';
+
+	return false;
+
+}
+
+void switchPlayers(int &turn)
+{
+	if (turn == 1)
+		turn = 2;
+	else
+		turn = 1;
+}
+/************************************************************************
+ * 
+ * FUNCTION spaceOccupied
+ * 	
+ *-----------------------------------------------------------------------
+ * Function description
+ *
+ *-----------------------------------------------------------------------
+ * PRE-CONDITIONS
+ * 		- ship length must not exceed the boundaries of the board
+ *
+ * POST-CONDITIONS
+ *
+*************************************************************************/
+bool spaceOccupied(PlayerBoard &p, 
+		           int row, 
+			 	   int col, 
+				   char orientation, 
+				   int shipSize)
+{
+	// check vertical placement
+	// TODO: checking for out of bounds can be done sooner
+	if (orientation == 'v')
+	{
+		for (int i = 0; i <shipSize; i++)
+		{
+			if  ((p.board[row][col] != UNKNOWN) ||
+				row > 9)
+				return true;
+			row++;
+		}
+	}
+
+	// check horizontal placement
+	else if (orientation == 'h')
+	{
+		for (int i = 0; i <shipSize; i++)
+		{
+			if ((p.board[row][col] != UNKNOWN) ||
+				col > 9)
+				return true;
+			col++;
+		}
+	}
+
+	return false;
+}
+
+void setShip(PlayerBoard & p, 
+		     int shipIndex)
+{
+	for (int i = 0; i < p.fleet[shipIndex].space.size(); i++)
+	{
+		int r = p.fleet[shipIndex].space[i].rows;
+		int c = p.fleet[shipIndex].space[i].cols;
+		p.board[r][c] = 'S';
+	}
+}
+
+void getValidShipInfo(PlayerBoard & p,
+					  int shipIndex)
+{
+	char ch; 			// INPUT - row as char first before conversion
+	int row, col;  		// INPUT - row and col for ship position
+	char orientation;	// INPUT - ship orientation
+
+
+
+	do 
+	{
+		// INPUT - get necessary values to place ships
+		cout << "Enter the starting coordinates of your "
+			 << p.fleet[shipIndex].name << ": ";
+		cin >> ch >> col;
+		row = ch - 65;
+		col = col - 1;
+		cout << endl;
+
+		cout << "Enter the orientation of your carrier "
+			 << "(horizontal(h) or vertical(v)): ";
+		cin >> orientation;
+
+		if (spaceOccupied(p,
+						  row,
+						  col,
+						  orientation,
+						  p.fleet[shipIndex].size))
+	    {
+			cout << "Error: Ship placement is outside the board.";
+			cout << endl;
+	    }
+	}
+	while (spaceOccupied(p,
+				         row,
+						 col,
+						 orientation,
+						 p.fleet[shipIndex].size));
+
+
+	// determine which spaces will be taken
+	// and save to PlayerBoard space
+	if (orientation == 'v')
+	{
+		for (int i = 0; i < p.fleet[shipIndex].size; i++)
+		{
+			p.fleet[shipIndex].space.push_back({row, col});
+			row++;
+		}
+	}
+
+	else if (orientation == 'h')
+	{
+		for (int i = 0; i < p.fleet[shipIndex].size; i++)
+		{
+			p.fleet[shipIndex].space.push_back({row, col});
+			col++;
+		}
+
+	}
+	setShip(p, shipIndex);
+	
+
+}
+
+void initFleet(PlayerBoard & p)
+{
+	p.fleet[0].name = "Carrier";
+	p.fleet[0].size = CARRIER_SIZE;
+	p.fleet[0].hitCount = 0;
+
+	p.fleet[1].name = "Battleship";
+	p.fleet[1].size = BATTLESHIP_SIZE;
+	p.fleet[1].hitCount = 0;
+
+	p.fleet[2].name = "Cruiser";
+	p.fleet[2].size = CRUISER_SIZE;
+	p.fleet[2].hitCount = 0;
+
+	p.fleet[3].name = "Submarine";
+	p.fleet[3].size = SUBMARINE_SIZE;
+	p.fleet[3].hitCount = 0;
+
+	p.fleet[4].name = "Destroyer";
+	p.fleet[4].size = DESTROYER_SIZE;
+	p.fleet[4].hitCount = 0;
+
+}
+
+
+int convertAscii(int row)
+{
+	return row - 65;
 }
 
 // initialize the board
-void initBoards(char playerHidden[][SIZE],
-		        char playerReveal[][SIZE],
-			    char compHidden[][SIZE],
-			    char compReveal[][SIZE])
+void initBoard(char p1[][SIZE],
+			   char p2[][SIZE])
 {
 	for (int i = 0; i < SIZE; i++)
 		for (int j = 0; j < SIZE; j++)
 		{
-			playerHidden[i][j] = UNKNOWN;
-			playerReveal[i][j] = UNKNOWN;
-			compHidden[i][j]   = UNKNOWN;
-			compHidden[i][j]   = UNKNOWN;
+			p1[i][j] = UNKNOWN;
+			p2[i][j]   = UNKNOWN;
 		}
 }
 
-void displayBoards(char playerReveal[][SIZE],
-			       char compReveal[][SIZE])
+void displayBoards(char p1[][SIZE],
+			       char p2[][SIZE])
 {
 	// initialize row title
 	char row = 'A';
@@ -84,7 +408,7 @@ void displayBoards(char playerReveal[][SIZE],
 		for (int j = 0; j < SIZE; j++)
 		{
 			cout << setw(2) << setfill(' ') << left;
-			cout << playerReveal[i][j] << "|";
+			cout << p1[i][j] << "|";
 			cout << " ";
 		}
 
@@ -96,7 +420,7 @@ void displayBoards(char playerReveal[][SIZE],
 		for (int j = 0; j < SIZE; j++)
 		{
 			cout << setw(2) << setfill(' ') << left;
-			cout << compReveal[i][j] << "|";
+			cout << p2[i][j] << "|";
 			cout << " ";
 		}
 
@@ -110,4 +434,12 @@ void displayBoards(char playerReveal[][SIZE],
 		cout << string(41, '-');
 		cout << endl;
 	}
+}
+
+
+void clearScreen()
+{ 
+	cout << endl;
+	cout << "\033c";
+	cout << endl;
 }
--- final_project/battle_ship.h
+++ final_project/battle_ship.h
@@ -1,30 +1,100 @@
 #include <iostream>
 #include <iomanip>
+#include <vector>
 using namespace std;
 
-char UNKNOWN = '3';
+char UNKNOWN = ' ';
+char MISS = 'M';
+char HIT = 'H';
 const int SIZE = 10;
-enum Ship
+const int FLEET_SIZE = 5;
+const int TOTAL_HIT_COUNT = 17;
+const int CARRIER_SIZE = 5;
+const int BATTLESHIP_SIZE = 4;
+const int CRUISER_SIZE = 3;
+const int SUBMARINE_SIZE = 3;
+const int DESTROYER_SIZE = 2;
+int p1TotalHitCount = 0;
+int p2TotalHitCount = 0;
+
+struct Point
 {
-	CARRIER = 5,
-	BATTLESHIP = 4,
-	CRUISER = 3,
-	SUBMARINE = 3,
-	DESTROYER = 2
+	int rows;
+	int cols;
+};
+
+struct Ship
+{
+	string name;
+	int size;
+	int hitCount;
+	vector<Point> space;
+};
+
+struct PlayerBoard
+{
+	char board[10][10];
+	Ship fleet[FLEET_SIZE];
 };
 
 
-void initBoards(char playerHidden[][SIZE],
-		       char playerReveal[][SIZE],
-			   char compHidden[][SIZE],
-			   char compReveal[][SIZE]);
+/**********************************************************************
+ * initBoard
+ * 		takes in two PlayerBoard objects by reference and calls the 
+ * 		setShip function for each ship in fleet
+**********************************************************************/
+void initBoard(char p1[][SIZE],
+			   char p2[][SIZE]);
 
-void displayBoards(char playerReveal[][SIZE],
-			   char compHidden[][SIZE]);
+void displayBoards(char player[][SIZE],
+			       char comp[][SIZE]);
 
-void initFleet();
-void setShip();
-void getValidShipInfo();
-bool spaceOccupid();
+/**********************************************************************
+ * setShip
+ * 		Takes in a PlayerBoard object by reference and an int variable
+ * 		that stores the index of the ship that is currently being 
+ * 		placed, and places the ship onto the board. Calls the
+ * 		getValidShipInfo function to determine which spots on the 
+ * 		board the ship will occupy.
+**********************************************************************/
+void setShip(PlayerBoard &, 
+		     int shipIndex);
+
+/**********************************************************************
+ * initFleet
+ * 		This function takes a PlayerBoard object by reference and calls
+ * 		the setShip function for each ship in the fleet.
+**********************************************************************/
+void initFleet(PlayerBoard &);
+
+void getValidShipInfo(PlayerBoard &,
+					  int shipIndex);
+
+bool spaceOccupied(PlayerBoard &, 
+		           int row, 
+			 	   int col, 
+				   char orientation, 
+				   int shipSize);
+void switchPlayers(int &turn);
 
 
+
+/**********************************************************************
+ * convertAscii
+ * 		converts Ascii letter to int row
+**********************************************************************/
+int convertAscii(int row);
+
+void clearScreen();
+
+void fireShot(PlayerBoard &,
+		      PlayerBoard &, 
+			  int turn);
+
+bool isHit(PlayerBoard &, int row, int col);
+
+int targetHit(PlayerBoard &,
+		      int row,
+			  int col);
+
+bool gameOver(PlayerBoard &, PlayerBoard &);
