bebf7e6645e1cf37d11991e235a6a48ba191871c
Eric Yun <eyun89@gmail.com>
2019-11-12 08:20:36 -0800

    / ‘cs_3a/assignments/08_inheritance/date.h’

---
+++ cs_3a/assignments/08_inheritance/date.h
@@ -0,0 +1,20 @@
+class Date
+{
+	private:
+		unsigned day;
+		unsigned month;
+		unsigned monthName;
+		unsigned year;
+		bool isLeap(unsigned) const;
+		unsigned daysPerMonth(unsigned m, unsigned y) const;
+		string name(unsigned m) const;
+		unsigned number(const string &mn) const;
+
+	
+	public:
+		Date();
+		Date(unsigned m, unsigned d, unsigned y);
+		Date(const string &mn, unsigned d, unsigned y);
+		void printNumeric() const;
+		void printAlpha() const;
+}
