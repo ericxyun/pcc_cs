a7dd86161385c34c018f52a16541d6e27ffeb28c
Eric Yun <eyun89@gmail.com>
2019-11-12 08:23:21 -0800

    / ‘cs_3a/assignments/08_inheritance/date.cpp’

---
+++ cs_3a/assignments/08_inheritance/date.cpp
@@ -0,0 +1,25 @@
+/** 
+ * PRIVATE FUNCTIONS
+ */
+bool Date::isLeap(unsigned) const
+{}
+unsigned Date::daysPerMonth(unsigned m, unsigned y) const
+{}
+string Date::name(unsigned m) const
+{}
+unsigned Date::number(const string &mn) const
+{}
+
+/** 
+ * PUBLIC FUNCTIONS
+ */
+Date::Date()
+{}
+Date::Date(unsigned m, unsigned d, unsigned y)
+{}
+Date::Date(const string &mn, unsigned d, unsigned y)
+{}
+void Date::printNumeric() const
+{}
+void Date::printAlpha() const
+{}
