efe2197ac8c239992d4305dce176201fcd9b42b2
Eric Yun <eyun89@gmail.com>
2019-11-12 08:33:35 -0800

    / ‘cs_3a/assignments/08_inheritance/date.cpp’

--- cs_3a/assignments/08_inheritance/date.cpp
+++ cs_3a/assignments/08_inheritance/date.cpp
@@ -4,6 +4,8 @@
 Date::Date()
 {
 	day = 1;
+	month = 1;
+	cout << name[1] << endl;
 
 }
 Date::Date(unsigned m, unsigned d, unsigned y)
@@ -15,7 +17,22 @@ bool Date::isLeap(unsigned) const
 unsigned Date::daysPerMonth(unsigned m, unsigned y) const
 {}
 string Date::name(unsigned m) const
-{}
+{
+	string months[12] = {
+		"January",
+		"February",
+		"March",
+		"April",
+		"May",
+		"June",
+		"July",
+		"August",
+		"September",
+		"October",
+		"November",
+		"December"}
+	return months[m]
+}
 unsigned Date::number(const string &mn) const
 {}
 
