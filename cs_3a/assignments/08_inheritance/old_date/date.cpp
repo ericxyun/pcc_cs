fc09372ddef35cfc5d374e605ebe4513c35182e6
Eric Yun <eyun89@gmail.com>
2019-11-12 08:46:34 -0800

    / ‘cs_3a/assignments/08_inheritance/date.cpp’ / ‘cs_3a/assignments/08_inheritance/date.h’ + ‘cs_3a/assignments/08_inheritance/old_date/date.cpp’ + ‘cs_3a/assignments/08_inheritance/old_date/date.h’

---
+++ cs_3a/assignments/08_inheritance/old_date/date.cpp
@@ -0,0 +1,65 @@
+#include "date.h"
+/** 
+ * PRIVATE FUNCTIONS
+ */
+Date::Date()
+{
+	day = 1;
+	month = 1;
+	monthName = name(month);
+	year = 2000;
+}
+Date::Date(unsigned m, unsigned d, unsigned y)
+{
+	day = d;
+	month = m;
+	monthName = name(m);
+	year = y;
+}
+Date::Date(const string &mn, unsigned d, unsigned y)
+{
+	day = d;
+	month = number(mn);
+	cout << month;
+	monthName = mn;
+	year = y;
+}
+bool Date::isLeap(unsigned) const
+{}
+unsigned Date::daysPerMonth(unsigned m, unsigned y) const
+{}
+string Date::name(unsigned m) const
+{
+	string months[12] = {
+		"January",
+		"February",
+		"March",
+		"April",
+		"May",
+		"June",
+		"July",
+		"August",
+		"September",
+		"October",
+		"November",
+		"December"};
+
+	return months[m];
+}
+unsigned Date::number(const string &mn) const
+{
+	for (int i = 0; i < 12; i++)
+		if (mn == name(month))
+			return i+1;
+	return 0;
+}
+
+/** 
+ * PUBLIC FUNCTIONS
+ */
+void Date::printNumeric() const
+{
+
+}
+void Date::printAlpha() const
+{}
