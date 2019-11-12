65bba0443d699a3c062362d04e3b26ee6ac2a93a
Eric Yun <eyun89@gmail.com>
2019-11-12 09:04:36 -0800

    / ‘cs_3a/assignments/08_inheritance/a.out’ / ‘cs_3a/assignments/08_inheritance/cs003AStudent.h’ / ‘cs_3a/assignments/08_inheritance/student.h’

---
+++ cs_3a/assignments/08_inheritance/cs003AStudent.h
@@ -0,0 +1,12 @@
+#include "date.h"
+class CS003AStudent: public Student
+{
+	private:
+		int totalScore;
+		bool knowledgeJava;
+		Date gradDate;
+	public:
+		CS003AStudent();
+		CS003AStudent();
+		print();
+};
---
+++ cs_3a/assignments/08_inheritance/student.h
@@ -0,0 +1,15 @@
+#include <string>
+
+class Student
+{
+	private:
+		string name;
+		int studentId;
+		int phoneNumber;
+		int age;
+		string gender;
+		string standing;
+		int gpa;
+	public:
+		print();
+};
