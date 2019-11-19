3f1b7108141a0c3dd30f27c3367df188ea69ff3f
Eric Yun <eyun89@gmail.com>
2019-11-19 08:49:06 -0800

    / ‘cs_3a/lab/16_class_templates/matrix.h’

--- cs_3a/lab/16_class_templates/matrix.h
+++ cs_3a/lab/16_class_templates/matrix.h
@@ -62,6 +62,8 @@ class Matrix
 
 		Matrix operator+(const Matrix &m)
 		{
+			Matrix<T> newMatrix;
+			newMatrix.array = array;
 			for (int i = 0; i < MAXROWS; i++)
 			{
 				for (int j = 0; j < MAXCOLS; j++)
