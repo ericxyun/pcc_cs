a54a23d2fd5447dfb49be681d662ef35e3ef0fdd
Eric Yun <eyun89@gmail.com>
2019-11-19 09:31:12 -0800

    / ‘cs_3a/lab/16_class_templates/main.cpp’

--- cs_3a/lab/16_class_templates/main.cpp
+++ cs_3a/lab/16_class_templates/main.cpp
@@ -6,10 +6,15 @@ using namespace std;
 int main()
 {
 	cout << "Demonstrating with string matrix:\n\n";
+
 	cout << "Matrix set to first array\n";
 	string firstArray[MAXROWS + MAXCOLS + 1] = {"Congra", "y", "ar", "alm", "don", "La"};
 	Matrix<string> m1;
 	m1.setMatrix(firstArray);
 	cout << m1;
+
+	cout << "Matrix incremented by second array\n\n";
+	string secondArray[MAXROWS + MAXCOLS + 1] = {"tulations", "ou", "e", "ost", "e", "b!"};
+	
 }
 
