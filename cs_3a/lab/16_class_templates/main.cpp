acb843c84f18aa553e5de0384f2578bbd98659c0
Eric Yun <eyun89@gmail.com>
2019-11-19 09:26:27 -0800

    / ‘cs_3a/lab/16_class_templates/main.cpp’

--- cs_3a/lab/16_class_templates/main.cpp
+++ cs_3a/lab/16_class_templates/main.cpp
@@ -7,7 +7,5 @@ int main()
 	cout << "Demonstrating with string matrix:\n\n";
 	cout << "Matrix set to first array\n";
 	string firstArray[MAXROWS + MAXCOLS + 1] = {"Congra", "y", "ar", "alm", "don", "La"};
-	Matrix<string>m1;
-	m1.setMatrix(firstArray);
 }
 
