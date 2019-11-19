d9ab63b9092667e9c4f5ee31d60a519efcf89758
Eric Yun <eyun89@gmail.com>
2019-11-19 08:16:31 -0800

    / ‘cs_3a/lab/16_class_templates/a.out’ / ‘cs_3a/lab/16_class_templates/matrix.cpp’

--- cs_3a/lab/16_class_templates/matrix.cpp
+++ cs_3a/lab/16_class_templates/matrix.cpp
@@ -8,7 +8,6 @@ Matrix<T>::Matrix()
 		for (int j = 0; j < MAXCOLS; j++)
 		{
 			array[i][j] = 0;
-			cout << array[i][j];
 		}
 	}
 }
@@ -16,5 +15,14 @@ Matrix<T>::Matrix()
 template <class T>
 void Matrix<T>::printMatrix() const
 {
+
+	for (int i = 0; i < MAXROWS; i++)	
+	{
+		for (int j = 0; j < MAXCOLS; j++)
+		{
+			cout << array[i][j];
+		}
+		cout << endl;
+	}
 }
 
