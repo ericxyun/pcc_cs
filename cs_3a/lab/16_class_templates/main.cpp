365dc7aab80bc0ef56c525e028b6d4a09fe8956a
Eric Yun <eyun89@gmail.com>
2019-11-19 09:20:23 -0800

    / ‘cs_3a/lab/16_class_templates/a.out’ / ‘cs_3a/lab/16_class_templates/main.cpp’

--- cs_3a/lab/16_class_templates/main.cpp
+++ cs_3a/lab/16_class_templates/main.cpp
@@ -4,9 +4,10 @@ using namespace std;
 
 int main()
 {
-	Matrix<int> m;
 	Matrix<int> n;
 	n.setElement(1, 2, 3);
+
+	Matrix<int> m;
 	int array[2][3] = {{1, 2, 3},
 						{4, 5, 6}};
 	m.setMatrix(array);
