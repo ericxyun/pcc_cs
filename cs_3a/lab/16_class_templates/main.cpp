fbf2f624cd7956e1b33d27ccde26aea46b79fb42
Eric Yun <eyun89@gmail.com>
2019-11-19 09:24:09 -0800

    / ‘cs_3a/lab/16_class_templates/a.out’ / ‘cs_3a/lab/16_class_templates/main.cpp’

--- cs_3a/lab/16_class_templates/main.cpp
+++ cs_3a/lab/16_class_templates/main.cpp
@@ -4,8 +4,7 @@ using namespace std;
 
 int main()
 {
-	cout << "Demonstrating with string matrix:\n";
-
+	cout << "Demonstrating with string matrix:\n\n";
 	cout << "Matrix set to first array\n";
 	string firstArray[MAXROWS + MAXCOLS + 1] = {"Congra", "y", "ar", "alm", "don", "La"};
 }
