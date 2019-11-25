5f03c2e19e983802df27687cc3566f203ccfbd6c
Eric Yun <eyun89@gmail.com>
2019-11-25 11:19:50 -0800

    / ‘cs_3a/assignments/09_linked_list_templates_exceptions/linkedList.h’ / ‘cs_3a/assignments/09_linked_list_templates_exceptions/listEmpty.h’

--- cs_3a/assignments/09_linked_list_templates_exceptions/linkedList.h
+++ cs_3a/assignments/09_linked_list_templates_exceptions/linkedList.h
@@ -12,5 +12,25 @@ class LinkedList
 		Node *head;
 		Node *tail;
 	
-	public;
+	public:
+		LinkedList()
+		LinkedList(const LinkedList& source)
+		~LinkedList()
+		LinkedList & operator=( const LinkedList& source )
+		void display() const
+		void push_front( const E& value )
+		void push_back( const E& value )
+		void pop_front() throw(ListEmpty)
+		const E& front() const throw(ListEmpty);
+		const E& back() const throw(ListEmpty);
+		void clear();
+		void select_sort()
+		void insert_sorted( const E& value )
+		void remove_duplicates()
+		Iterator begin()
+		Iterator end()
+		int length() const;
+		int sum() const;
+		bool isEmpty() const;  
+
 }
