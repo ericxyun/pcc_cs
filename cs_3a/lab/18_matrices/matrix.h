49efa4d22b8704920e2fad6cb887b54709a50471
Eric Yun <eyun89@gmail.com>
2019-11-26 08:43:40 -0800

    / ‘cs_3a/lab/18_matrices/matrix.h’

--- cs_3a/lab/18_matrices/matrix.h
+++ cs_3a/lab/18_matrices/matrix.h
@@ -24,7 +24,7 @@ class MatrixMisMatchException: public invalid_argument
 		{
 			return get_message.c_str();
 		}
-		const*char get_message(string);
+		const *char* get_message(string);
 	protected:
 		string message;
 
